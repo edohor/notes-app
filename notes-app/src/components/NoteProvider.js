c78a2ee01550e1eee95a6c48ff615a723e48027b
edohor <edohorvat01@gmail.com>
2020-09-04 15:45:16 +0200

    functional delete and edit note

--- notes-app/src/components/NoteProvider.js
+++ notes-app/src/components/NoteProvider.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import "./Note.css"
 
 let noteList = [];
+let newNoteClicked = true;
 
 function NoteProvider (props) {
 
@@ -10,51 +11,92 @@ function NoteProvider (props) {
         (localStorage.getItem("notes")!==null && JSON.parse(localStorage.getItem("notes")).length>0) ? 
         JSON.parse(localStorage.getItem("notes")) : []
         );
+    const [noteId, setNoteId] = useState(-1);
+
+    console.log("localStorage = ", JSON.parse(localStorage.getItem("notes")));
 
     noteList = notes;
 
     const [modal, setModal] = useState(false);
+    const [content, setContent] = useState("");
 
-    // const notes = useNotes();
+    function addNote(content) {
+        console.log("[addNote] content = ", content);
 
-    // const ids = notes.getAllIds();
-    // const note = notes.get(id);
-    // notes.save(id, source);
-    // notes.add(initialSource);
-    // notes.remove(id);
+        // TEST
+        let largestId = 0;
+        console.log("[addNote] noteList = ", noteList);
+        if(noteList.length>0){
+            let checkIds = noteList.reduce((a, c) => (a[c.id] = c, a), {});
     
-    // notes.open(id);
-    // notes.close();
-    // const selectedId = notes.getSelectedId();
-    // const selectedNote = notes.getSelected();
+            //Object looks like:
+            console.log("test", checkIds);
+    
+            // find the largest key // maybe a good way to decide the next key
+            largestId = Math.max(...Object.keys(checkIds))+1;
+            console.log("largest ID", largestId);
+        }
 
-    function addNote() {
-        let obj = {value: noteList.length, label: "General Kenobi"};
+
+
+        let obj = {id: largestId, content: content};
         // obj[noteList.length] = "General Kenobi";
         noteList.push(obj);
         console.log("new noteList = ", noteList);
 
         localStorage.setItem("notes", JSON.stringify(noteList));
 
-        setNotes(JSON.parse(localStorage.getItem("notes")))
+        setNotes(JSON.parse(localStorage.getItem("notes")));
+        
+        newNoteClicked = false;
+    
+        setNoteId(largestId)
     }
 
-    function deleteNote(index) {
-        console.log("index = ", index);
+    function editNote(content) {
+        console.log("[editNote] content = ", content);
+        console.log("[editNote] noteId = ", noteId);
+        
+        let obj = {id: noteId, content: content};
 
-        noteList.splice(index, 1);
+        for(let i=0; i<noteList.length; i++){
+            if(noteList[i].id === noteId){
+                console.log("noteList [i] = ", noteList[i]);
+                noteList.splice(i, 1, obj);
+            }
+        }
+
+        localStorage.setItem("notes", JSON.stringify(noteList));
+
+        setNoteId(-1);
+        setNotes(JSON.parse(localStorage.getItem("notes")));
+    }
+
+    function deleteNote(deleteNodeId) {
+        console.log("[deleteNote] deleteNodeId = ", deleteNodeId);
+
+        for(let i=0; i<noteList.length; i++){
+            if(noteList[i].id === deleteNodeId){
+                console.log("noteList [i] = ", noteList[i]);
+                noteList.splice(i, 1);
+            }
+        }
         console.log("noteList after delete = ", noteList);
 
         localStorage.setItem("notes", JSON.stringify(noteList));
 
-        setNotes(JSON.parse(localStorage.getItem("notes")))
+        setNoteId(-1);
+        setNotes(JSON.parse(localStorage.getItem("notes")));
+        closeModalWindow();
     }
 
     function openModalWindow() {
+        newNoteClicked = true;
         setModal(true);
     }
 
     function closeModalWindow() {
+        setContent("")
         setModal(false);
     }
 
@@ -62,14 +104,25 @@ function NoteProvider (props) {
         setModal(false);
     }
 
+    function openNote(openedNoteId, index) {
+        console.log("[openNote] openedNoteId = ", openedNoteId);
+        console.log("[openNote] index = ", index);
+        setContent(notes[index].content);
+        setNoteId(openedNoteId)
+        console.log("[openNote] noteId = ", noteId);
+
+        newNoteClicked = false;
+        setModal(true);
+    }
+
     console.log("notes = ", notes);
 
     let displayNotes = [];
     if (notes.length>0){
         displayNotes = notes.map((note, index) => {
-            console.log("note = ", note);
+            // console.log("note = ", note);
             return <div className="note"
-                        onClick={() => deleteNote(index)}>{note.label}</div>
+                        onClick={() => openNote(note.id, index)}>{note.content}</div>
         })
     }
 
@@ -78,13 +131,19 @@ function NoteProvider (props) {
             <div class="modalWindow">
                 <div className="tools">
                     <span class="close" onClick={closeModalWindow}>&larr;</span>
-                    <span class="delete" onClick={deleteNote}>Delete</span>
-                    <span class="edit" onClick={enableEditNote}>Save</span>
+                    <span class="delete" onClick={() => deleteNote(noteId)}>Delete</span>
+                    <span class="edit" onClick={newNoteClicked ? 
+                        () => addNote(content) : () => editNote(content)}>Save</span>
                 </div>
-                <textarea className="textInput">Some text in the Modal..</textarea>
+
+                <textarea className="textInput"
+                onChange={e => setContent(e.target.value)}
+                value={content}>{content}</textarea>
             </div>
         </div>
     )
+
+    console.log("content = ", content);
 
     return (
         <div className="noteContainer">
